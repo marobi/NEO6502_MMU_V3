f783f5a3989f2ffc6481814dd3c34dbfafe95384
Marobi <rien.matthijsse@gmail.com>
2025-08-10 13:40:34 +0200

    Replace absolute ROM includes with embedded images

--- NEO6502_MMU_V3/rom_bios.h
+++ NEO6502_MMU_V3/rom_bios.h
@@ -1,4 +1,17 @@
 #pragma once
 
-#include "D:\cygwin64\home\Rien\neo6502\bios_mmu\bios.mon"
+// Placeholder BIOS ROM image
+static const uint8_t bios_bin[] = {
+    0x5A, 0x01, 0x01,        // SOH, minor, major
+    0x00, 0x02,              // start address 0x0200
+    0x02, 0x00,              // size 2 bytes
+    0x06,                    // type: RESET+IRQ vectors
+    0x00, 0x00,              // NMI vector
+    0x00, 0x02,              // RESET vector
+    0x00, 0x02,              // IRQ vector
+    0x0E,                    // checksum
+    0xA5,                    // EOH
+    // Code bytes
+    0xEA, 0xEA               // NOP; NOP
+};
 
--- NEO6502_MMU_V3/rom_test.h
+++ NEO6502_MMU_V3/rom_test.h
@@ -1,4 +1,17 @@
 #pragma once
 
-#include "D:\cygwin64\home\Rien\neo6502\test_mmu\test.mon"
+// Simple test ROM image with minimal program
+static const uint8_t test_bin[] = {
+    0x5A, 0x01, 0x01,        // SOH, minor, major
+    0x00, 0x02,              // start address 0x0200
+    0x01, 0x00,              // size 1 byte
+    0x06,                    // type: RESET+IRQ vectors
+    0x00, 0x00,              // NMI vector
+    0x00, 0x02,              // RESET vector
+    0x00, 0x02,              // IRQ vector
+    0x0D,                    // checksum
+    0xA5,                    // EOH
+    // Code bytes
+    0xEA                     // NOP
+};
 
